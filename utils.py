2ef3d05b7aca3ece530b65101f821d67538b23ae
Denny Britz <dennybritz@gmail.com>
2015-09-28 11:48:05 +0200

    Content, model saving, and generating text.

--- utils.py
+++ utils.py
@@ -3,3 +3,17 @@ import numpy as np
 def softmax(x):
     xt = np.exp(x - np.max(x))
     return xt / np.sum(xt)
+
+def save_model_parameters_theano(outfile, model):
+    U, V, W = model.U.get_value(), model.V.get_value(), model.W.get_value()
+    np.savez(outfile, U=U, V=V, W=W)
+    print "Saved model parameters to %s." % outfile
+   
+def load_model_parameters_theano(path, model):
+    npzfile = np.load(path)
+    U, V, W = npzfile["U"], npzfile["V"], npzfile["W"]
+    model.U.set_value(U)
+    model.V.set_value(V)
+    model.W.set_value(W)
+    print "Loaded model parameters from %s." % path
+    
