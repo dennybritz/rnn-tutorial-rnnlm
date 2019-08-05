e09c0b7c088c650a82eb32155663ffbad4f77e3a
AnNguyenUT <39999966+thienan99dt@users.noreply.github.com>
2019-08-04 21:18:05 -0700

    Add files via upload
    Change code Python 2 to Python 3

--- train-theano.py
+++ train-theano.py
@@ -30,11 +30,11 @@ def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evalu
             loss = model.calculate_loss(X_train, y_train)
             losses.append((num_examples_seen, loss))
             time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-            print "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss)
+            print("%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss))
             # Adjust the learning rate if loss increases
             if (len(losses) > 1 and losses[-1][1] > losses[-2][1]):
                 learning_rate = learning_rate * 0.5  
-                print "Setting learning rate to %f" % learning_rate
+                print("Setting learning rate to %f" % learning_rate)
             sys.stdout.flush()
             # ADDED! Saving model oarameters
             save_model_parameters_theano("./data/rnn-theano-%d-%d-%s.npz" % (model.hidden_dim, model.word_dim, time), model)
@@ -51,21 +51,21 @@ sentence_end_token = "SENTENCE_END"
 
 # Read the data and append SENTENCE_START and SENTENCE_END tokens
 print "Reading CSV file..."
-with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+with open('data/reddit-comments-2015-08.csv', 'rt', encoding = 'utf8') as f:
     reader = csv.reader(f, skipinitialspace=True)
     reader.next()
     # Split full comments into sentences
-    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].lower()) for x in reader])
     # Append SENTENCE_START and SENTENCE_END
     sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
-print "Parsed %d sentences." % (len(sentences))
+print("Parsed %d sentences." % (len(sentences)))
     
 # Tokenize the sentences into words
 tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
 
 # Count the word frequencies
 word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-print "Found %d unique words tokens." % len(word_freq.items())
+print("Found %d unique words tokens." % len(word_freq.items()))
 
 # Get the most common words and build index_to_word and word_to_index vectors
 vocab = word_freq.most_common(vocabulary_size-1)
@@ -73,8 +73,8 @@ index_to_word = [x[0] for x in vocab]
 index_to_word.append(unknown_token)
 word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
 
-print "Using vocabulary size %d." % vocabulary_size
-print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+print("Using vocabulary size %d." % vocabulary_size)
+print("The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
 
 # Replace all words not in our vocabulary with the unknown token
 for i, sent in enumerate(tokenized_sentences):
@@ -89,7 +89,7 @@ model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
 t1 = time.time()
 model.sgd_step(X_train[10], y_train[10], _LEARNING_RATE)
 t2 = time.time()
-print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
+print("SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.))
 
 if _MODEL_FILE != None:
     load_model_parameters_theano(_MODEL_FILE, model)
